b03d7265cf07273048bf5b91347d79ec12e09926
Wang Xuerui <idontknw.wang@gmail.com>
2012-04-10 01:05:15 +0800

    add file-level docstring w/ a title

--- weiyu/rendering/base.py
+++ weiyu/rendering/base.py
@@ -17,6 +17,15 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+u'''
+Base classes for rendering
+~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+This module provides the basic object rendering infrastructure, with
+both pre-rendering and post-rendering hooks.
+
+'''
+
 from __future__ import unicode_literals, division
 
 from .exc import RenderingError
