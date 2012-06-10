ce64388896e97d7e38fc12a9efe8602f52c63fa4
Wang Xuerui <idontknw.wang@gmail.com>
2012-06-11 02:25:20 +0800

    restore __future__ statement in case of future expansion

--- kbslib/sitecfg.py
+++ kbslib/sitecfg.py
@@ -17,6 +17,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import unicode_literals, division
+
 
 IDLEN, NAMELEN, OLDPASSLEN, MD5PASSLEN = 12, 40, 14, 16
 FILENAME_LEN, OWNER_LEN, ARTICLE_TITLE_LEN = 20, 14, 60
