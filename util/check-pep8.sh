9e0d8e1db79e6a6d889067ab576f82ccf90329e5
Wang Xuerui <idontknw.wang@gmail.com>
2012-06-12 19:24:20 +0800

    shortcut for invoking the PEP8 conformance checker

---
+++ util/check-pep8.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+UTIL_DIR="$( cd "$( dirname ${BASH_SOURCE[0]} )" && pwd )"
+
+cd "${UTIL_DIR}/.."
+
+for subdir in weiyu kbslib; do
+    find "${subdir}" -name '*.py' -type f | xargs pep8
+done
+
+
+# vim:ai:et:ts=4:sw=4:sts=4:fenc=utf8:
