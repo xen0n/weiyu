648d40d3b7657a9ed589ae6503a486201c63aba0
Wang Xuerui <idontknw.wang@gmail.com>
2012-07-10 17:12:11 +0800

    add hub

--- weiyu/db/__init__.py
+++ weiyu/db/__init__.py
@@ -17,5 +17,43 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import unicode_literals, division
+
+__all__ = [
+        'Hub',
+        ]
+
+from ..registry.classes import UnicodeRegistry
+from ..registry.provider import request
+
+HANDLERS_KEY = 'drivers'
+
+
+class DatabaseHub(object):
+    # TODO: refactor this and RenderHub to use common helper code
+
+    def __init__(self):
+        self._reg = request(
+                'weiyu.db',
+                autocreate=True,
+                klass=UnicodeRegistry,
+                )
+
+        if HANDLERS_KEY not in self._reg:
+            self._reg[HANDLERS_KEY] = {}
+
+    def register_handler(self, typ):
+        def _decorator_(fn):
+            self._reg[HANDLERS_KEY] = fn
+            return fn
+        return _decorator_
+
+    def get_database(self, typ, *args, **kwargs):
+        factory_func = self._reg[HANDLERS_KEY][typ]
+        return factory_func(*args, **kwargs)
+
+
+Hub = DatabaseHub()
+
 
 # vim:set ai et ts=4 sw=4 sts=4 fenc=utf-8:
