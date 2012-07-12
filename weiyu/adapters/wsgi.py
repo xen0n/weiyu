12e1b9473f4332e56537afa2fe5758fe556315f5
Wang Xuerui <idontknw.wang@gmail.com>
2012-07-12 17:38:51 +0800

    well... silly typo

--- weiyu/adapters/wsgi.py
+++ weiyu/adapters/wsgi.py
@@ -152,7 +152,7 @@ class WSGIReflex(BaseReflex):
             # TODO: It's apparent that we need a smart_str helper here!!
             bytes_k = k.encode(enc) if issubclass(type(k), unicode) else k
             bytes_v = v.encode(enc) if issubclass(type(v), unicode) else v
-            headers.append((k, v, ))
+            headers.append((bytes_k, bytes_v, ))
 
         start_response(status_line, headers)
 
