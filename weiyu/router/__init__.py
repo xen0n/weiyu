1fd19de7616c2d31012af70847b94c9aeac5c210
Wang Xuerui <idontknw.wang@gmail.com>
2012-07-13 10:04:12 +0800

    add router initializer from list of rules; fix some apparent mistakes

--- weiyu/router/__init__.py
+++ weiyu/router/__init__.py
@@ -23,8 +23,6 @@ __all__ = [
         'router_hub',
         ]
 
-from .base import RouterBridge
-
 from ..helpers.hub import BaseHub
 from ..registry.classes import UnicodeRegistry
 
@@ -62,22 +60,60 @@ class RouterHub(BaseHub):
     def register_router(self, router):
         # keep a reference to the router
         typ = router.name
-        self._router_map[typ] = router
+        if typ is None:
+            raise ValueError(
+                    'only named routers can be registered this way'
+                    )
+
+        self._routers[typ] = router
 
         # also reserve a slot in endpoints dict, if one is not already
         # set up
         if typ not in self._endpoints:
             self._endpoints[typ] = {}
 
-        # create a bridge class to do the dispatch work
-        bridge = RouterBridge(router)
-        # register its dispatch method as handler, and we're done
-        self.register(typ, bridge.dispatch)
+        # register the router's dispatch method as handler, and we're done
+        self.register_handler(typ)(router.dispatch)
 
     def dispatch(self, typ, querystr, *args):
         # typically used with args=(request, ) inside the framework
         # TODO: is it really useful to allow passing kwargs also?
         return self.do_handling(typ, querystr, *args)
+
+    def _do_init_router(self, typ, routing_rules, cls, lvl):
+        # recursive algorithm, watch out d-:
+        # typ is not really useful except checking against endpoint reg
+        #
+        # let's construct the desired target initializer out of the
+        # pattern-to-(endpoint-or-router) list
+        _list_types = (list, tuple, )
+        _str_types = (str, unicode, )
+
+        result_rules = []
+        for pattern, target_spec in routing_rules:
+            if isinstance(target_spec, _list_types):
+                # this is a router... recursively construct a router out
+                # of it
+                tgt = self._do_init_router(typ, target_spec, cls, lvl + 1)
+            elif isinstance(target_spec, _str_types):
+                # target is endpoint... check against endpoint registry
+                # this is where typ is used
+                # enforce a little bit of encoding requirement, to make
+                # messed up encoding problem surface fast
+                tgt = self._endpoints[typ][unicode(target_spec)]
+            else:
+                # unrecognized target specification, pass it thru as is
+                tgt = target_spec
+
+            # add a rule
+            result_rules.append((pattern, tgt, ))
+
+        # construct a XxxRouter object with the routing rules just created
+        # if toplevel router, assign it a name equal to typ
+        return cls(result_rules, name=typ if lvl == 0 else None)
+
+    def init_router(self, typ, routing_rules, cls):
+        return self._do_init_router(typ, routing_rules, cls, 0)
 
 
 router_hub = RouterHub()
