9a971a20027dc1355c249bf376a3f4f78989345c
Wang Xuerui <idontknw.wang@gmail.com>
2012-07-17 13:13:42 +0800

    encoding cleanup

---
+++ weiyu/helpers/misc.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# weiyu / helpers / miscellaneous helpers
+#
+# Copyright (C) 2012 Wang Xuerui <idontknw.wang-at-gmail-dot-com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals, division
+
+__all__ = [
+        'smartstr',
+        'smartbytes',
+        ]
+
+
+def smartstr(s, encoding='utf-8', *args, **kwargs):
+    if not issubclass(type(s), unicode):
+        return unicode(s, enc, *args, **kwargs)
+    return s
+
+def smartbytes(s, encoding='utf-8', *args, **kwargs):
+    if issubclass(type(s), unicode):
+        return s.encode(encoding, *args, **kwargs)
+    return s
+
+
+# vim:set ai et ts=4 sw=4 sts=4 fenc=utf-8:
