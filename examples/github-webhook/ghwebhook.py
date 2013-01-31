8c56cac9abc2bf5294400513743490a03951e7d3
Wang Xuerui <idontknw.wang@gmail.com>
2013-01-31 18:39:49 +0800

    WIP: github post-receive webhook listener

---
+++ examples/github-webhook/ghwebhook.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# weiyu / examples / GitHub webhook listener
+#
+# Copyright (C) 2013 Wang Xuerui <idontknw.wang-at-gmail-dot-com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals, division
+
+try:
+    import ujson as json
+except ImportError:
+    import json
+
+from weiyu.reflex.classes import ReflexResponse
+
+from weiyu.registry.provider import request as regrequest
+from weiyu.rendering.decorator import renderable
+from weiyu.router import router_hub
+
+# Valid GitHub callback IPs
+GH_IP_WHITELIST = (
+        '207.97.227.253',
+        '50.57.128.197',
+        '108.171.174.178',
+        '50.57.231.61',
+        )
+
+
+def dummy_response(status):
+    return ReflexResponse(
+            status,
+            None,
+            {
+                'mimetpe': 'text/plain',
+                },
+            request,
+            )
+
+
+@router_hub.endpoint('http', 'gh-webhook-post-receive')
+@renderable('dummy')
+def on_gh_post_receive(request):
+    if request.remote_addr not in GH_IP_WHITELIST:
+        return dummy_response(403)
+
+    if request.method != 'POST':
+        # TODO: a limit method decorator would be better
+        return dummy_response(400)
+
+    try:
+        payload_json = request.form['payload']
+    except KeyError:
+        return dummy_response(400)
+
+    try:
+        payload = json.loads(payload_json)
+    except ValueError:
+        return dummy_response(400)
+
+    # TODO: WIP
+
+    return dummy_response(204)
+
+
+# vim:ai:et:ts=4:sw=4:sts=4:ff=unix:fenc=utf-8:
---
+++ weiyu/rendering/dummy.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# weiyu / rendering / dummy renderer
+#
+# Copyright (C) 2013 Wang Xuerui <idontknw.wang-at-gmail-dot-com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+u'''
+Dummy renderer
+--------------
+
+This renderer, as its name suggests, does not render anything. This is
+useful when implementing certain API endpoints that do not or must not
+return any content.
+
+'''
+
+from __future__ import unicode_literals, division
+
+__all__ = ['DummyRenderable', ]
+
+from . import render_hub
+from .base import Renderable
+
+
+class DummyRenderable(Renderable):
+    def _do_render(self, result, context):
+        # Intentionally left blank
+        return b'', {}
+
+
+@render_hub.register_handler('dummy')
+def dummy_render_handler(hub, name, *args, **kwargs):
+    # template name is meaningless, thus ignored.
+    return DummyRenderable(*args, **kwargs)
+
+
+# vim:set ai et ts=4 sw=4 sts=4 fenc=utf-8:
