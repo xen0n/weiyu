2ea5b22b2baa9d44c4b3be7709a4417732e8e615
Wang Xuerui <idontknw.wang@gmail.com>
2013-03-06 20:07:39 +0800

    Update whitelist of GitHub WebHook servers

--- weiyu/utils/ghwebhook.py
+++ weiyu/utils/ghwebhook.py
@@ -24,25 +24,28 @@ try:
 except ImportError:
     import json
 
+try:
+    import ipaddress
+except ImportError:
+    import ipaddr as ipaddress
+
 from weiyu.reflex.classes import ReflexResponse
 
 from weiyu.registry.provider import request as regrequest
 from weiyu.rendering.decorator import renderable
 from weiyu.router import router_hub
 
-# Valid GitHub callback IPs
+# Valid GitHub callback IP ranges
 GH_IP_WHITELIST = (
-        '207.97.227.253',
-        '50.57.128.197',
-        '108.171.174.178',
-        '50.57.231.61',
-        '54.235.183.49',
-        '54.235.183.23',
-        '54.235.118.251',
-        '54.235.120.57',
-        '54.235.120.61',
-        '54.235.120.62',
+        '207.97.227.253/32',
+        '50.57.128.197/32',
+        '108.171.174.178/32',
+        '50.57.231.61/32',
+        '204.232.175.64/27',
+        '192.30.252.0/22',
         )
+
+GH_NETWORKS = [ipaddress.IPv4Network(i) for i in GH_IP_WHITELIST]
 
 
 def dummy_response(status, request):
@@ -56,13 +59,18 @@ def dummy_response(status, request):
             )
 
 
+def is_ip_whitelisted(ip):
+    addr = ipaddress.IPv4Address(ip)
+    return any(addr in net for net in GH_NETWORKS)
+
+
 @router_hub.endpoint('http', 'gh-webhook-post-receive')
 @renderable('dummy')
 def on_gh_post_receive(request):
     conf = regrequest('site')['github']['post-receive']
     dummy = lambda status: dummy_response(status, request)
 
-    if request.remote_addr not in GH_IP_WHITELIST:
+    if not is_ip_whitelisted(request.remote_addr):
         return dummy(403)
 
     if request.method != 'POST':
