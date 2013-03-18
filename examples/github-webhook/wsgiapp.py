9c57f14ca3c060f4e2e5c50778289b784897809c
Wang Xuerui <idontknw.wang@gmail.com>
2013-03-18 10:37:21 +0800

    examples/github-webhook: fix copyright year

--- examples/github-webhook/wsgiapp.py
+++ examples/github-webhook/wsgiapp.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # weiyu / examples / GitHub WebHook API host - WSGI file
 #
-# Copyright (C) 2012 Wang Xuerui <idontknw.wang-at-gmail-dot-com>
+# Copyright (C) 2013 Wang Xuerui <idontknw.wang-at-gmail-dot-com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
