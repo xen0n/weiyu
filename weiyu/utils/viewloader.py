58677dab4f7dcbd859cef11dd4617e6881904d29
Wang Xuerui <idontknw.wang@gmail.com>
2013-04-02 08:58:50 +0800

    utils: Add view loader
    This is an auto-importer for modules, designed to move all the import
    statements out of your adapter file. It can be configured in JSON, and
    can do either absolute imports or relative imports based on a package
    specified.
    
    The modules specified in the `modules` key are loaded in order,
    optionally with the package specified by `package` as anchor for
    relative import. If `package` key is absent, or converts to `None`, the
    module would only do absolute imports (this is exactly the same behavior
    the underlying `importlib.import_module` exhibits).

---
+++ weiyu/utils/viewloader.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# weiyu / utilities / view loader
+#
+# Copyright (C) 2013 Wang Xuerui <idontknw.wang-at-gmail-dot-com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals, division
+
+__all__ = [
+        'ViewLoader',
+        ]
+
+import importlib
+import json
+
+
+class ViewLoader(object):
+    def __init__(self, path):
+        with open(path, 'rb') as fp:
+            content = fp.read()
+        self.config = json.loads(content)
+
+    def __call__(self):
+        cfg = self.config
+        pkg_anchor = cfg.get('package', None)
+        modules = cfg.get('modules', [])
+
+        for name in modules:
+            importlib.import_module(name, pkg_anchor)
+
+
+# vim:set ai et ts=4 sw=4 sts=4 fenc=utf-8:
