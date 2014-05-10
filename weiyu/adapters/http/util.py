5c8cfa5b9d3472c2a6c5674308900d93d40fa975
Wang Xuerui <idontknw.wang@gmail.com>
2014-05-10 18:36:53 +0800

    adapters/http/util: typos

--- weiyu/adapters/http/util.py
+++ weiyu/adapters/http/util.py
@@ -183,9 +183,9 @@ def _parse_multipart_form(stream, length, options):
             # XXX: see multipart's equivalent part for caveats applicable here
             val = part.value
             try:
-                forms[part_name].append(val)
+                form[part_name].append(val)
             except KeyError:
-                forms[part_name] = [val, ]
+                form[part_name] = [val, ]
 
     compact_multidict_inplace(form)
     compact_multidict_inplace(files)
@@ -309,7 +309,7 @@ class HTTPHelper(object):
     def maybe_parse_payload_into(self, request):
         # TODO: allow setting custom verbs?
         if request.method in {'POST', 'PUT', }:
-            request.form, request.files = self._do_parse_content(request)
+            request.form, request.files = self._do_parse_payload(request)
 
     def _init_sts(self, config):
         if not config.get('enabled', True):
